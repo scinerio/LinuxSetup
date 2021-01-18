da9ba9cc107100903fa7dbf928cf5cb4ccae6cb6
scinerio <scinerio2@gmail.com>
2021-01-18 14:59:53 -0600

    Organizing

--- setup.sh
+++ setup.sh
@@ -1,57 +1,58 @@
 #!/bin/bash
 
-echo "Installing the coolest packages ever"
-
+echo "Updating apt"
 sudo apt update
 
-sudo apt install -y python3-pip gobuster zsh 
+echo "Installing the coolest packages ever"
+
+sudo apt install -y python3-pip gobuster zsh terminator
 
 # Pip installations
 #wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 #python /tmp/get-pip.py
 #rm /tmp/get-pip.py
 
-# Impacket download
+echo "Making directories in /opt"
 mkdir /opt/impacket
-git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
-pip3 install -r /opt/impacket/requirements.txt
-pip3 install pysmb
-python3 /opt/impacket/setup.py install
-
-# Pentester Framework
 mkdir /opt/ptf
-git clone https://github.com/trustedsec/ptf.git /opt/ptf
-pip install -r /opt/ptf/requirements.txt 
-
-# AutoRecon for CTF things
-git clone https://github.com/Tib3rius/AutoRecon.git /opt/AutoRecon
-pip3 install -r /opt/AutoRecon/requirements.txt
-
-# Color themes for Terminator
-sudo apt install -y terminator
-pip install requests
 mkdir -p $HOME/.config/terminator/plugins
-wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes.py"
+mkdir /opt/pwncat
+mkdir /opt/ffuf
 
+echo "Cloning git repos"
+# Impacket 
+git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
+# Pentester Framework
+git clone https://github.com/trustedsec/ptf.git /opt/ptf
+# AutoRecon
+git clone https://github.com/Tib3rius/AutoRecon.git /opt/AutoRecon
+# Color themes for Terminator
+wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes.py"
 # ZSH Autosuggestion plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Install oh-my-zsh
+# oh-my-zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# Pwncat
+git clone https://github.com/calebstewart/pwncat.git /opt/pwncat
+# FFUF
+git clone https://github.com/ffuf/ffuf.git /opt/ffuf
 
+echo "Installing dependencies"
+python3 /opt/impacket/setup.py install
+pip3 install -r /opt/impacket/requirements.txt
+pip install -r /opt/ptf/requirements.txt 
+pip3 install -r /opt/AutoRecon/requirements.txt
+pip3 install pysmb
+pip install requests
+
+
+echo "Linking syms"
 # Symlink ZSH config for root user
 ln -s $HOME/.oh-my-zsh           /root/.oh-my-zsh
 ln -s $HOME/.zshrc               /root/.zshrc
 
-# Get Pwncat
-mkdir /opt/pwncat
-git clone https://github.com/calebstewart/pwncat.git /opt/pwncat
-
-# Get FFUF
-mkdir /opt/ffuf
-git clone https://github.com/ffuf/ffuf.git /opt/ffuf
 
 
-# Set ZSH as default shell
+echo "Changing default shell to zsh"
 sudo chsh -s $(which zsh)
 
